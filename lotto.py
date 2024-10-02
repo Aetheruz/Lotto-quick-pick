46b79e5af0bf8ab3c4f3f8a1cb40c75911b81e7f
Sssstoic <jamesromar.tuling@edu.sait.ca>
2024-10-02 15:17:22 -0600

    added daily grand

--- lotto.py
+++ lotto.py
@@ -6,7 +6,7 @@ choice = input("Did you want to play lotto? (yes/no): ")
 i = choice.lower()
 
 while i == "yes" and i != "no":
-    max_49 = input("Press 1 for 6/49 or 2 for Lotto Max and 3 for exit: ")
+    max_49 = input("Press 1 for 6/49 or 2 for Lotto Max and 3 for Daily Grand and 4 for exit: ")
     if max_49 == "1":
         print("You choose 6/49")
         lotto_49 = random.sample(range(1, 50), 6)
@@ -16,6 +16,10 @@ while i == "yes" and i != "no":
         lotto_max = random.sample(range(1, 50), 7)
         print(lotto_max)
     elif max_49 == "3":
+        print("You choose Lotto Max")
+        lotto_max = random.sample(range(1, 50), 5)
+        print(lotto_max)
+    elif max_49 == "4":
         i = "no"
     else:
         print("You choose wrong number")
